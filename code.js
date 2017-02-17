2e010c18f2e1118bdbd3fbd49bfea5ac2eb8ec5f
Ali Alrawi <ali.developer90@gmail.com>
2017-02-17 13:38:14 +0300

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,14 @@
 'use strict';
+function favoriteIceCream() {
+	// body...
+	return "I love mint chocolate chip"
+}
+
+function shouting(x){
+   return x.toUpperCase();
+}
+function roundDown(n){
+	return Math.floor(n)
+}
+
+function theTruth(){return true ;}
